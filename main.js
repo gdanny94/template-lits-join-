8058cca06014e12be5e6c5b183576deedca8afa5
gdanny94 <109629790+gdanny94@users.noreply.github.com>
2024-11-18 16:20:04 -0500

    template lits + join()

---
+++ main.js
@@ -0,0 +1,11 @@
+
+function sayHello( name, city, state ) {
+    //create string with template lits
+    //return str
+    
+    return `Hello, ${name.join(' ')}! Welcome to ${city}, ${state}!`
+    
+    
+  }
+  
+  console.log(sayHello(['Millie','Garcia'], 'Phonenix', 'Arizona'))
